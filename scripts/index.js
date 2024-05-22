7e6f1c30fb2aac26b21c49590daa689d42bdc1e7
Martin Ramos <ramoslalo94@gmail.com>
2024-05-22 00:26:34 -0300

    Declaro_variables

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,19 @@
 /*
  * APELLIDO, NOMBRE | APELLIDO, NOMBRE
  */
-
+//Declaro las variables
+let disco, artista, cod_disco, cancion, duracion;
 /**
  * Llamada desde un boton. Pide los datos para un disco.
  */
 function cargar() {
     // TODO:
+    disco=prompt("ingrese nombre del disco: ");
+    artista=prompt("ingrese el nombre del artista / banda: ");
+    cod_disco=Number(prompt("ingrese codigo del disco: "));
+    cancion=prompt("ingrese el nombre de la cancion: ");
+    duracion=prompt("ingrese la duracion de la pista: ");
+       
 }
 
 /**
